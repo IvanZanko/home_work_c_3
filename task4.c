f6f6e8085f3f26a36818e3ef489c6f59b28262f2
IvanZanko <zankoivan88@gmail.com>
2024-01-03 22:04:00 +0600

    Mine home work

---
+++ task4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    int month;
+
+    printf("Enter the month number (от 1 до 12): ");
+    scanf("%d", &month);
+
+    switch (month) {
+        case 12:
+        case 1:
+        case 2:
+            printf("Winter\n");
+            break;
+        case 3:
+        case 4:
+        case 5:
+            printf("Spring\n");
+            break;
+        case 6:
+        case 7:
+        case 8:
+            printf("Summer\n");
+            break;
+        case 9:
+        case 10:
+        case 11:
+            printf("Autumn\n");
+            break;
+        default:
+            printf("Inccorect month entry\n");
+    }
+
+    return 0;
+}
